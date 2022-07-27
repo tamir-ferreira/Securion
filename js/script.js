9f3ff57a364eb4fb55742977813d8a8f1a13fdb9
Danilotm <52943817+Danilotm@users.noreply.github.com>
2022-07-26 21:13:16 -0300

    inclusão da página de contato

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const toggleButton = document.querySelector('.navbar-toggle');
+const navbarLinks = document.getElementsByClassName('navbar-links');
+toggleButton.addEventListener('click', function() {
+    for (let index = 0; index < navbarLinks.length; index++) {
+        navbarLinks[index].classList.toggle('active');
+        
+    }
+});
