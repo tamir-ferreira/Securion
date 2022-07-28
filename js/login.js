f689c206baabdc739a134fd6f3416842515a287a
Pedro Santos <ggpedrosantos@gmail.com>
2022-07-28 19:25:36 -0300

    adiciona script para validar login

---
+++ js/login.js
@@ -0,0 +1,16 @@
+function validate() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+	if (username == "aluno" && password == "123aluno") {
+		alert("Aluno logado com sucesso!");
+		window.location.href = "./feed.html";
+	} else {
+		if (username == "diretor" && password == "123diretor") {
+			alert("Diretor logado com sucesso!");
+			window.location = "./feed-adm.html";
+			return false;
+		} else {
+			alert("Usuário e/ou senha inválidos.");
+		}
+	}
+}
