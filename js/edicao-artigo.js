06742cb6d7ec625586b71a73503ad66e8153bf51
Gabriela Jenyffer <gabjenramos@gmail.com>
2022-07-28 15:38:02 -0300

    Adiciona tela de edição

---
+++ js/edicao-artigo.js
@@ -0,0 +1,26 @@
+const corpoArt = document.querySelector('#corpoArt');
+const tituloArt = document.querySelector('#tituloArt');
+
+//Preenche as caixas de texto editáveis
+function fillContent(element, sentence) {
+ 	if(element.innerHTML.length == 0) {
+    	element.innerHTML = `${sentence}`;
+    }
+}
+
+//Apaga o texto padrão se o usuário não digitar nada
+function toggleContent(element,sentence) {
+    element.addEventListener('focus', () => {
+    	if(element.innerHTML == sentence) {
+    		element.innerHTML = '';
+    	}
+    });
+
+    element.addEventListener('blur', () => fillContent(element, sentence));
+}
+
+fillContent(tituloArt, 'Título');
+fillContent(corpoArt, 'Digite seu texto...');
+
+toggleContent(tituloArt, 'Título');
+toggleContent(corpoArt, 'Digite seu texto...');
