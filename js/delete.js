8b60090ecee3599fef5b78eadee8ed050a32bfe1
Pedro Santos <ggpedrosantos@gmail.com>
2022-07-29 16:26:27 -0300

    adiciona script em artigos  e retire linhas desnecessarias do css

---
+++ js/delete.js
@@ -0,0 +1,7 @@
+document.getElementById("deleteArticle1").onclick = function () {
+	document.getElementById("article1").style.display = "none";
+};
+
+document.getElementById("deleteArticle2").onclick = function () {
+	document.getElementById("article2").style.display = "none";
+};
