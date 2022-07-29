8685b777e00c25157e1d636e2e52fd43707978d0
Pedro Santos <ggpedrosantos@gmail.com>
2022-07-29 01:18:25 -0300

    adiciona tela de cadastrar alunos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const toggleButton = document.querySelector('.navbar-toggle');
-const navbarLinks = document.getElementsByClassName('navbar-links');
-toggleButton.addEventListener('click', function() {
-    for (let index = 0; index < navbarLinks.length; index++) {
-        navbarLinks[index].classList.toggle('active');
-        
-    }
+const toggleButton = document.querySelector(".navbar-toggle");
+const navbarLinks = document.getElementsByClassName("navbar-links");
+toggleButton.addEventListener("click", function () {
+	for (let index = 0; index < navbarLinks.length; index++) {
+		navbarLinks[index].classList.toggle("active");
+	}
 });
+
+date.max = new Date().toISOString().split("T")[0];
